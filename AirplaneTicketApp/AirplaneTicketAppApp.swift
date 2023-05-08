671e74926c7a3721b499fa4db4c73b920fc8c31b
hanh vo <hahnvo422@gmail.com>
2023-05-08 10:26:13 -0500

    Initial Commit

---
+++ AirplaneTicketApp/AirplaneTicketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirplaneTicketAppApp.swift
+//  AirplaneTicketApp
+//
+//  Created by Hanh Vo on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AirplaneTicketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
